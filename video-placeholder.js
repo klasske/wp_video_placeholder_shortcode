b5b258cc2f5f1a4a51e5c8365d35b25724be1bcc
Klaske Van Vuurden <klaske.van.vuurden@uit.no>
2015-03-25 14:58:33 +0100

    Correct iframe code

--- video-placeholder.js
+++ video-placeholder.js
@@ -7,9 +7,13 @@ jQuery(document).ready(function(){
     jQuery('img.video_placeholder').on('click', function(){
     	// get the video-src attribute and replace the current item with an iframe
     	var video_src = jQuery(this).data("video-src");
+    	var width = jQuery(this).width();
+    	var height = jQuery(this).height();
     	
-    	jQuery(this).attr("src", video_src);
     	
+    	jQuery(this).replaceWith(
+    			"<iframe src='" + video_src + "' width=" + width + " height=" + height + " frameborder='0'></iframe>"
+    			);
     	
     });
 
